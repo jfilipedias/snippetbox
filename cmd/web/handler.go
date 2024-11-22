4b76dce57cee7fedad1459a5c383d66ca381300d
Filipe Dias <filipediascontato@gmail.com>
2024-11-22 14:58:52 -0300

    feat: adds authentication middleware

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -200,5 +200,13 @@ func (app *application) loginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) logoutPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Logout the user")
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+	app.sessionManager.Put(r.Context(), "flash", "You're been logged out successfully!")
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -16,14 +16,16 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
-	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
-	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
-
 	mux.Handle("GET /signup", dynamic.ThenFunc(app.signup))
 	mux.Handle("POST /signup", dynamic.ThenFunc(app.signupPost))
 	mux.Handle("GET /login", dynamic.ThenFunc(app.login))
 	mux.Handle("POST /login", dynamic.ThenFunc(app.loginPost))
-	mux.Handle("POST /logout", dynamic.ThenFunc(app.logoutPost))
+
+	protected := dynamic.Append(app.requireAuthentication)
+
+	mux.Handle("GET /snippet/create", protected.ThenFunc(app.snippetCreate))
+	mux.Handle("POST /snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	mux.Handle("POST /logout", protected.ThenFunc(app.logoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 	return standard.Then(mux)
