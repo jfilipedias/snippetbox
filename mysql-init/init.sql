23c81ddc1b030a56d1a9e2afdbd038a83bbbfa48
Filipe Dias <filipediascontato@gmail.com>
2024-11-28 11:32:00 -0300

    test: create user model integration test

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -2,6 +2,14 @@ DROP DATABASE IF EXISTS snippetbox;
 
 CREATE DATABASE snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+DROP DATABASE IF EXISTS test_snippetbox;
+
+CREATE DATABASE test_snippetbox CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE USER 'test_web'@'%';
+GRANT CREATE, DROP, ALTER, INDEX, SELECT, INSERT, UPDATE, DELETE ON test_snippetbox.* TO 'test_web'@'%';
+ALTER USER 'test_web'@'%' IDENTIFIED BY 'pass';
+
 USE snippetbox;
 
 CREATE TABLE snippets (
